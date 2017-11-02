6030b50240f14f5d544956941241577c18c962d0
codeshakti <codeshaktiman@gmail.com>
2017-11-02 11:29:55 +0100

    api component created

---
+++ my-app/scripts/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from 'main'
+
+const App = () => (
+    <div>
+        <form
+
+    </div>
+)
+
+export default App
---
+++ my-app/scripts/todoAPI.jsx
@@ -0,0 +1,35 @@
+const todoApi = {
+	baseUrl: 'http://localhost:3001/',
+
+	listTodos: function() {
+		return axios.get(this.baseUrl + '/list-todos')
+		.then(function({data}){
+			return data.data
+		})
+	},
+
+	createTodos: function(todo, state) {
+		return axios.post(this.baseUrl + '/create-todos')
+		.then(function({data}){
+			return data.data
+		})
+	},
+
+	changeTodos: function(id, todo, state) {
+		return axios.put(this.baseUrl + '/change-todos/' + id)
+		.then(function({data}){
+			return data.data
+		})
+	},
+
+	deleteTodos: function(id) {
+		return axios.delete(this.baseUrl + '/delete-todos' + id)
+		.then(function({data}){
+			return data.data
+		})
+	}
+
+}
+
+export default todoApi
+
